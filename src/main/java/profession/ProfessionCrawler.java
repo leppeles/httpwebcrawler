2dde77617b22792f2d389e695f3fcfeeb6efde62
leppeles <19oszi91@gmail.com>
2018-07-05 22:56:26 +0200

    Progress on profcrawler to excel (not finished)

--- src/main/java/profession/ProfessionCrawler.java
+++ src/main/java/profession/ProfessionCrawler.java
@@ -3,12 +3,20 @@ package profession;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.HashSet;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -24,6 +32,8 @@ public class ProfessionCrawler {
 	int noOfPage = 0;
 	private HashSet<String> visitedURLs;
 	private static final int BUFFER_SIZE = 4096;
+	private static Workbook workbook;
+	private static final String sheetName = "Joblist";
 	private static final SimpleDateFormat dateformat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 	String rootURL;
 
@@ -38,16 +48,62 @@ public class ProfessionCrawler {
 	public void run(String globalProfURL, String localProfURL, String saveTo) {
 		log.info("~~~~~~~~~~~~~~~~~~~~HTTP crawler started~~~~~~~~~~~~~~~~~~~~");
 		saveParentDir = saveTo;
-		getJobs(globalProfURL, localProfURL.substring(1));
+		workbook = createExcel();
+		loopThrough(globalProfURL, localProfURL.substring(1));
 	}
 
-	private void getJobs(String linkFirstPart, String linkSecondPart) {
+	private Workbook createExcel() {
+		Workbook workbook = new XSSFWorkbook();
 
-		int n = 0;
-		while (isExistingSite(linkFirstPart, linkSecondPart, n)) {
+		Sheet sheet = workbook.createSheet(sheetName);
+		sheet.setColumnWidth(0, 6000);
+		sheet.setColumnWidth(1, 4000);
 
-			n++;
-		}
+		Row header = sheet.createRow(0);
+
+		CellStyle headerStyle = workbook.createCellStyle();
+		headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+		font.setFontName("Arial");
+		font.setFontHeightInPoints((short) 16);
+		font.setBold(true);
+		headerStyle.setFont(font);
+
+		Cell headerCell = header.createCell(0);
+		headerCell.setCellValue("Title");
+		headerCell.setCellStyle(headerStyle);
+
+		headerCell = header.createCell(1);
+		headerCell.setCellValue("Salary");
+		headerCell.setCellStyle(headerStyle);
+
+		headerCell = header.createCell(2);
+		headerCell.setCellValue("Location");
+		headerCell.setCellStyle(headerStyle);
+
+		headerCell = header.createCell(3);
+		headerCell.setCellValue("Link");
+		headerCell.setCellStyle(headerStyle);
+
+		headerCell = header.createCell(4);
+		headerCell.setCellValue("Description");
+		headerCell.setCellStyle(headerStyle);
+
+		CellStyle style = workbook.createCellStyle();
+		style.setWrapText(true);
+
+		return workbook;
+
+	}
+
+	private void loopThrough(String linkFirstPart, String linkSecondPart) {
+
+		// int noOfPages = 0;
+		// while (isExistingSite(linkFirstPart, linkSecondPart, noOfPages)) {
+		// noOfPages++;
+		// }
 
 		for (int i = 0; i < 1; i++) {
 
@@ -56,7 +112,7 @@ public class ProfessionCrawler {
 			tempURL.append(i);
 			tempURL.append(linkSecondPart);
 			try {
-				getListOfJobs(tempURL);
+				extractEntities(tempURL);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -65,11 +121,11 @@ public class ProfessionCrawler {
 
 	}
 
-	private boolean isExistingSite(String linkFirstPart, String linkSecondPart, int siteCount) {
+	private boolean isExistingSite(String linkFirstPart, String linkSecondPart, int noOfPages) {
 
 		StringBuilder tempURL = new StringBuilder();
 		tempURL.append(linkFirstPart);
-		tempURL.append(siteCount);
+		tempURL.append(noOfPages);
 		tempURL.append(linkSecondPart);
 
 		URL url;
@@ -87,7 +143,7 @@ public class ProfessionCrawler {
 		return responseCode == HttpURLConnection.HTTP_OK;
 	}
 
-	private void getListOfJobs(StringBuilder tempURL) throws IOException {
+	private void extractEntities(StringBuilder tempURL) throws IOException {
 		URL url = new URL(tempURL.toString());
 		HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
 		int responseCode = httpConn.getResponseCode();
@@ -118,82 +174,59 @@ public class ProfessionCrawler {
 					System.out.print(jobLink.isEmpty() ? "" : jobLink.get(0).attr("href"));
 					System.out.print(" ----- ");
 					System.out.println(jobDescription.hasText() ? jobDescription.get(0).text() : "");
+
+					Workbook workbook = new XSSFWorkbook();
+
+					Sheet sheet = workbook.createSheet("temp");
+					sheet.setColumnWidth(0, 6000);
+					sheet.setColumnWidth(1, 4000);
+
+					Row header = sheet.createRow(0);
+
+					CellStyle headerStyle = workbook.createCellStyle();
+					headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+					headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+					XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+					font.setFontName("Arial");
+					font.setFontHeightInPoints((short) 16);
+					font.setBold(true);
+					headerStyle.setFont(font);
+
+					Cell headerCell = header.createCell(0);
+					headerCell.setCellValue("Name");
+					headerCell.setCellStyle(headerStyle);
+
+					headerCell = header.createCell(1);
+					headerCell.setCellValue("Age");
+					headerCell.setCellStyle(headerStyle);
+
+					CellStyle style = workbook.createCellStyle();
+					style.setWrapText(true);
+
+					Row row = sheet.createRow(2);
+					Cell cell = row.createCell(0);
+					cell.setCellValue("John Smith");
+					cell.setCellStyle(style);
+
+					cell = row.createCell(1);
+					cell.setCellValue(20);
+					cell.setCellStyle(style);
+
+					File currDir = new File(saveParentDir);
+					String path = currDir.getAbsolutePath();
+					String fileLocation = path.substring(0, path.length() - 1) + "profSalaries.xlsx";
+
+					FileOutputStream outputStream = new FileOutputStream(fileLocation);
+					workbook.write(outputStream);
+					workbook.close();
+
 				}
 			}
 		} else {
 			log.error("No file to download. Server replied HTTP code: " + responseCode + "\n From site: " + tempURL);
 		}
 		httpConn.disconnect();
-	}
-
-	/**
-	 * Downloads a file from a URL
-	 * 
-	 * @param stringURL HTTP URL of the site to be downloaded
-	 * @param saveParentDir path of the directory to save the file
-	 * @throws IOException
-	 */
-	private static void downloadFile(String stringURL, String saveParentDir) throws IOException {
-		if (stringURL.endsWith("/")) {
-			stringURL = stringURL.substring(0, stringURL.length() - 1);
-		}
-		URL url = new URL(stringURL);
-		HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-		int responseCode = httpConn.getResponseCode();
-
-		// checks HTTP response code first
-		if (responseCode == HttpURLConnection.HTTP_OK) {
-
-			// Log content for each downloaded file built with StringBuffer
-			StringBuffer logContent = new StringBuffer();
-
-			String fileNameWithPath = createFileNameWithPath(stringURL, url, saveParentDir);
-
-			// opens input stream from the HTTP connection
-			InputStream inputStream = httpConn.getInputStream();
-
-			// opens an output stream to save into file
-			FileOutputStream outputStream = new FileOutputStream(fileNameWithPath);
-
-			int bytesRead = -1;
-			byte[] buffer = new byte[BUFFER_SIZE];
-			while ((bytesRead = inputStream.read(buffer)) != -1) {
-				outputStream.write(buffer, 0, bytesRead);
-			}
-
-			outputStream.close();
-			inputStream.close();
-
-		} else {
-			log.error("No file to download. Server replied HTTP code: " + responseCode + "\n From site: " + stringURL);
-		}
-		httpConn.disconnect();
-	}
-
-	/**
-	 * Builds string file name with path and creates location if not exists
-	 * 
-	 * @param stringURL
-	 * @param url
-	 * @param saveParentDir
-	 * @return the html filename with absolute path, including sub-dirs from URL structure
-	 */
-	private static String createFileNameWithPath(String stringURL, URL url, String saveParentDir) {
-		String fileName = "";
-		String dirSubPath = "";
-		saveParentDir = (saveParentDir + File.separator + url.getHost());
-
-		fileName = (stringURL.substring(stringURL.lastIndexOf("/") + 1, stringURL.length()) + (stringURL.endsWith(".html") ? "" : ".html"))
-				.replaceAll("[\\/:*?<|>]", "_");
-
-		dirSubPath = url.getPath().toString();
-		dirSubPath = (dirSubPath.substring(0, dirSubPath.lastIndexOf("/") + 1));
-		dirSubPath.replace("/", File.separator);
-
-		(new File(saveParentDir + dirSubPath)).mkdirs();
-		String fileNameWithPath = saveParentDir + dirSubPath + File.separator + fileName;
-
-		return fileNameWithPath;
 	}
 
 }
